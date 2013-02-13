cd7f06934d45dcad55b2d26d19d0ba59078ff044
Michele Nuijten <michele.nuijten@gmail.com>
2013-02-13 15:17:32 +0100

    added estimates of mean and median of posterior distribution to the plot.CI function

--- R/plot.CI.R
+++ R/plot.CI.R
@@ -3,8 +3,12 @@ plot.CI <- function(x,...){
        main=expression(paste("posterior distribution for ",alpha*beta,sep=" ")),
        xlab=expression(alpha*beta),...)
   abline(v=quantile(x,c(.025,.975)),lwd=2,col="red")
+  abline(v=mean(x),lwd=2,col="green")
+  abline(v=median(x),lwd=2,col="blue")
   legend("topright",
-         legend=paste(c("lower bound 95% CI","upper bound 95% CI"),
-                      round(quantile(x,c(.025,.975)),2),sep=": "),
-         col="red",lty=1,lwd=2)
+         legend=c(paste(c("lower bound 95% CI","upper bound 95% CI"),
+                      round(quantile(x,c(.025,.975)),3),sep=": "),
+                  paste("mean",round(mean(x),3),sep=": "),
+                  paste("median",round(median(x),3),sep=": ")),
+         col=c("red","red","green","blue"),lty=1,lwd=2)
 }
