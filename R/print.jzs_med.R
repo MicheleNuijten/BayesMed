1cdec01fa240dfac27c2cd9636130524736ba50b
MicheleNuijten <michele.nuijten@gmail.com>
2017-04-27 21:57:26 -0700

    don't print cor_coef_samples by default

--- R/print.jzs_med.R
+++ R/print.jzs_med.R
@@ -1,6 +1,6 @@
 print.jzs_med <-
 function(x,...){
-  print(x[!(names(x)%in%c("alpha_samples","beta_samples","tau_prime_samples","rho",
+  print(x[!(names(x)%in%c("cor_coef_samples","alpha_samples","beta_samples","tau_prime_samples","rho",
                           "prob_alpha","prob_beta","prob_tau_prime_samples",
                           "ab_samples","jagssamples","jagssamplesA","jagssamplesTB"))])
 }
